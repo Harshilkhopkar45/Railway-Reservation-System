a1d03c439d38254a97565c5d7d97bcc75e82f5c0
Harshil Khopkar <155041319+Harshilkhopkar45@users.noreply.github.com>
2025-06-21 16:37:58 +0530

    railway database.py

--- Code/railway database.py
+++ Code/railway database.py
@@ -1,6 +1,6 @@
 def Create_Database_Railway():
     import mysql.connector
-    mycon=mysql.connector.connect(host='localhost',user='root',passwd='hk45')    
+    mycon=mysql.connector.connect(host='localhost',user='root',passwd='')    
     cursor=mycon.cursor()
     mycon.autocommit=True
     s1="create database railway"
